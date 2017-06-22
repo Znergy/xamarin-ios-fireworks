108ba3fdf870d4cc1d1cd158702a80a12d57f69b
znergy <ryanjonesirl@gmail.com>
2017-06-21 23:14:13 -0700

    add code for a basic design app, using ios storyboard, modals, switch/button/slider, info light button, and also using the powerful ios constraints

---
+++ Fireworks/AboutViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Fireworks
+{
+    public partial class AboutViewController : UIViewController
+    {        partial void ButtonClose_TouchUpInside(UIButton sender) {            this.DismissViewController(true, null);        }
+
+        public AboutViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
